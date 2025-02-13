b0e6643977881813ee10cf32829cc9b3ae1f1630
Ranikadam722 <kadamrani722@gmail.com>
2025-02-13 20:52:45 +0530

    Add files via upload

---
+++ IpAddressFynder.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ip.address.fynder;
+
+public class IpAddressFynder {
+
+    public static void main(String[] args) {
+        System.out.println("IP Address Fynder !");
+    }
+}
